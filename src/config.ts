24b73cbea43aa7095094c40b21005691e3316fd4
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-03-07 17:06:53 -0500

    Get Heroku mongodb url

--- src/config.ts
+++ src/config.ts
@@ -22,6 +22,15 @@ export class MongoConfig {
   }
 }
 
-export let mongoConfig: MongoConfig = new MongoConfig('127.0.0.1', 32768, '', '', 'homework10');
+export let mongoConfig: MongoConfig;
+
+if (process.env.MONGODB_URI) {
+  let matches = process.env.MONGODB_URI.match('\\/\\/([a-zA-Z0-9_]+):([a-zA-Z0-9_]+)@([a-zA-Z0-9_\\.]+):([0-9]+)\\/([a-zA-Z0-9_]+)');
+  if (matches) {
+    mongoConfig = new MongoConfig(matches[2], parseInt(matches[3], 10), matches[0], matches[1], matches[4]);
+  }
+} else {
+  mongoConfig = new MongoConfig('127.0.0.1', 32768, '', '', 'homework10');
+}
 
 export let serverPort: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
