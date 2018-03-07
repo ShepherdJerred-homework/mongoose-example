c7adbee52491d03a72fde85031c37348325b7f7a
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-03-07 17:10:13 -0500

    Log

--- src/config.ts
+++ src/config.ts
@@ -28,6 +28,8 @@ if (process.env.MONGODB_URI) {
   let matches = process.env.MONGODB_URI.match('\\/\\/([a-zA-Z0-9_]+):([a-zA-Z0-9_]+)@([a-zA-Z0-9_\\.]+):([0-9]+)\\/([a-zA-Z0-9_]+)');
   if (matches) {
     mongoConfig = new MongoConfig(matches[2], parseInt(matches[3], 10), matches[0], matches[1], matches[4]);
+    console.log(matches);
+    console.log(mongoConfig);
   }
 } else {
   mongoConfig = new MongoConfig('127.0.0.1', 32768, '', '', 'homework10');
